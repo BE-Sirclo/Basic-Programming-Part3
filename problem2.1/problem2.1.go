5eb40448afdc95032b885d06b7854d91b6a47188
Iswanul Umam <iswanul.umam@gmail.com>
2021-12-15 05:37:40 +0700

    first commit

---
+++ problem2.1/problem2.1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func FaktorBilangan(n int) string {
+	// Do your code here
+}
+
+func main() {
+	var number int
+	fmt.Scanf("%d", &number)
+	fmt.Println(FaktorBilangan(number))
+}
---
+++ problem2.2/problem2.2.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func FaktorBilanganDesc(n int) string {
+	// write your code
+}
+
+func main() {
+	var number int
+	fmt.Scanf("%d", &number)
+	fmt.Println(FaktorBilanganDesc(number))
+}
